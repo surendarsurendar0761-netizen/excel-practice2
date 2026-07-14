3293095747ed4de74f4d7633f2fd0cd92028750e
surendarsurendar0761-netizen <surendarsurendar0761@gmail.com>
2026-07-14 20:52:32 +0530

    practice sql

---
+++ Sql/sql.sql
@@ -0,0 +1,15 @@
+create database bankingdb;
+show databases;
+use bankingdb;
+CREATE TABLE bankingdb
+(
+STUDENT_ID INT,
+STUDENT_NAME varchar(50),
+EMAIL VARCHAR(50),
+SCORES DECIMAL(5,2),
+JOINING_DATE date
+);
+
+show tables;
+
+describe bankingdb;
